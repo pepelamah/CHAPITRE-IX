54a8317a44d837ec8429d7a60d9316c07b0cc499
pepelamah <77665057+pepelamah@users.noreply.github.com>
2021-09-15 22:08:26 +0200

    Add files via upload

---
+++ exo2_chapitre9.php
@@ -0,0 +1,25 @@
+23 lines (18 sloc)  456 Bytes
+  
+<!DOCTYPE html> 
+ 
+<html> 
+<head> 
+<title>Titre de la page</title> 
+<meta charset=”utf-8”> 
+</head> 
+ 
+<body> 
+<?php 
+echo '<pre>'; 
+print_r($_POST); 
+echo '</pre>'; 
+if (($_POST['nom']=' ') OR ($_POST['prenom']=' ') OR ($_POST['adresse']=' ') OR ($_POST['ville']=' ') OR ($_POST['codepostal']=' '))
+{
+    echo"<script>alert('Le formulaire est incomplet');document.location='exo_2_chapitre9.html'</script>";
+}
+
+
+?> 
+</body> 
+ 
+</html> 
